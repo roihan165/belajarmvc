6187af9d611093c712299f990ed6f37d4842cf23
roihan165 <muhammadroihan165@gmail.com>
2024-03-20 09:45:09 +0700

    Menambahkan .htaccess dan route

--- app/core/App.php
+++ app/core/App.php
@@ -2,7 +2,24 @@
     class App{
         public function __construct()
         {
-            echo 'Berhasil Login ke class utama App!';
+            $url = $this->parseUrl();
+            var_dump($url);
+            echo ($url);
+        }
+
+        public function parseURL(){
+            if (isset($_GET['url'])){
+                // Ambil value url
+                $url = $_GET['url'];
+                // Buang tanda '/' di akhir url
+                $url = rtrim($url,'/');
+                // Filter url dari karakter aneh
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                // Pecal url berdasarkan tanda '/' sebagai pemisah
+                $url = explode('/',$url);
+
+                return $url;
+            }
         }
     }
 ?>
